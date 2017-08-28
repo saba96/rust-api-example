0cacda2c1d0a62ea0b79fb602da80b96a64fc92e
Saba Ahmadi <saba.ahmadi96@gmail.com>
2017-08-28 16:31:33 +0900

    add post method

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,37 @@
 #[macro_use]
 extern crate nickel;
 
-use nickel::{Nickel, JsonBody, HttpRouter, Request, Response, MiddlewareResult, MediaType};
+// use nickel::{Nickel, JsonBody, HttpRouter, Request, Response, MiddlewareResult, MediaType};
+
+
+extern crate rustc_serialize;
+
+#[macro_use(bson, doc)]
+extern crate bson;
+extern crate mongodb;
+
+// Nickel
+use nickel::{Nickel, JsonBody, HttpRouter};
+use nickel::status::StatusCode::{self};
+
+// MongoDB
+use mongodb::{Client, ThreadedClient};
+use mongodb::db::ThreadedDatabase;
+use mongodb::error::Result as MongoResult;
+
+// bson
+use bson::{Bson, Document};
+use bson::oid::ObjectId;
+
+// rustc_serialize
+use rustc_serialize::json::{Json, ToJson};
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct User {
+    firstname: String,
+    lastname: String,
+    email: String
+}
 
 fn main() {
 
@@ -16,9 +46,36 @@ fn main() {
 
     });
 
+    // router.post("/users/new", middleware! { |request, response|
+
+    //     format!("Hello from POST /users/new")
+
+    // });
     router.post("/users/new", middleware! { |request, response|
 
-        format!("Hello from POST /users/new")
+        // Accept a JSON string that corresponds to the User struct
+        let user = request.json_as::<User>().unwrap();
+
+        let firstname = user.firstname.to_string();
+        let lastname = user.lastname.to_string();
+        let email = user.email.to_string();
+
+        // Connect to the database
+        let client = Client::connect("localhost", 27017)
+            .ok().expect("Error establishing connection.");
+
+        // The users collection
+        let coll = client.db("rust-users").collection("users");
+
+        // Insert one user
+        match coll.insert_one(doc! {
+            "firstname" => firstname,
+            "lastname" => lastname,
+            "email" => email
+        }, None) {
+            Ok(_) => (StatusCode::Ok, "Item saved!"),
+            Err(e) => return response.send(format!("{}", e))
+        }
 
     });
 
